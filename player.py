d9ed0a4bc5c9194505817837f9847e29c1b0652b
Selisine <Aidenpike113@gmail.com>
2024-03-22 10:21:04 -0400

    Create player.py

---
+++ player.py
@@ -0,0 +1,15 @@
+import pygame as game
+
+class player:
+    def __init__(self, health, defense, damage):
+        self.health = health
+        self.defense = defense
+        self.damage = damage
+
+    def get_movement():
+        for event in game.event.get():
+            if event.type == game.K_w:
+                
+
+        
+    

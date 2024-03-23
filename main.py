0511d08b167bbc29b01f35a1d129985319d165c1
Selisine <Aidenpike113@gmail.com>
2024-03-23 09:24:09 +0000

    Import screen class, cast speed to int

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame as game
 from player import *
+from screen import *
 
 game.init()
-
-
 
 set_fps = game.time.Clock()
 game.display.set_caption("Roguelike")
 set_fps.tick(60)
 
-o_player = player(10, 5, 3, 1/5, 0, 0)
+o_player = player(10, 5, 3, int(1/5), 0, 0)
+
+resolution = game.display.set_mode([800, 600])
+background_color = (128, 128, 128)
+o_screen = screen(resolution, background_color)
 
 while True:
     for event in game.event.get():
         if event.type == game.QUIT:
             game.quit()
 
-    o_player.get_movement(screen)
+    o_player.get_movement()
+    o_player.rotate_player()
 
-    game.display.update()
+    o_screen.fill_screen()
 

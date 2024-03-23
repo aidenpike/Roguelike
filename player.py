46b3a599d9f8452c1982fc027319270fbaa15c8e
Selisine <Aidenpike113@gmail.com>
2024-03-23 09:06:09 +0000

    Add rotate_player()

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 import pygame as game
+import math 
 
+# Encompasses all player related controls 
 class player:
     def __init__(self, health, defense, damage, speed, pos_x, pos_y):
         self.health = health
@@ -9,6 +11,7 @@ class player:
         self.pos_x = pos_x
         self.pos_y = pos_y
 
+    # Moves player based on input
     def get_movement(self, screen):
         get_key = game.key.get_pressed()
 
@@ -21,9 +24,20 @@ class player:
         if get_key[game.K_d]:
             self.pos_x += self.speed
             
-
-        game.draw.circle(screen, (0,0,255), (self.pos_x, self.pos_y), 75)
+        game.draw.rect(screen, (0,0,255), 10, 5)
         #Expect screen class later. 
+
+    # Point player to cursor
+    # Needs debug later
+    def rotate_player(self):
+        mouse_x, mouse_y = game.mouse.get_pos()
+        rel_x, rel_y = mouse_x - self.pos_x, mouse_y - self.pos_y
+        angle = math.atan2(rel_y, rel_x)
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+
+        self.image = game.transform.rotate(self.original_image, int(angle))
+        self.rect = self.image.get_rect(center = self.position)
+
 
     def draw_player(self):
         self.get_movement()   
